19749794cca9797a6d3fac9411df3edc09f2e1b2
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-06-24 13:05:47 -0400

    Se añaden funciones randomColor() y pickColor() en js

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 //Variable html//
-
-const titulo= document.querySelector('h1')
+const titulo = document.querySelector('h1')
 const cuadrados = document.querySelectorAll('.square')
-const span_color= document.querySelector('#colorDisplay')
+const span_color = document.querySelector('#colorDisplay')
+let span_message = document.querySelector("#message")
 
 //variables globales//
 const colors = ['rgb(54, 135, 216)', 'rgb(48, 196, 33)', 'rgb(234, 237, 44)', 'rgb(233, 47, 28)', 'rgb(157, 57, 164)', 'rgb(191, 201, 202)']
-const pickedColor = colors[3]
+let pickedColor = undefined;
+let clickedColor = undefined;
 
-for (let i=0;i<6;i++) {
-  cuadrados[i].style['background-color']= colors[i]
+// Funciones
+function changeColors(color) {
+  for (let i = 0; i < cuadrados.length; i++) {
+    span_message.textContent = "¡Correcto!";
+    cuadrados[i].style['background-color'] = color;
+    titulo.style['color'] = color;
+    span_color.textContent = color;
+  }
 }
 
-titulo.style.color= pickedColor
-console.log(pickedColor)
-span_color.textContent= pickedColor
+// Funcion para retornar color aleatorio
+function pickColor() {
+  pickedColor = Math.floor(Math.random() * colors.length);
+  return pickedColor;
+}
+function randomColor() {
+  return pickedColor = colors[pickColor()];
+}
+
+randomColor();
+
+for (let i = 0; i < colors.length; i++) {
+  cuadrados[i].style['background-color'] = colors[i]
+}
+
+for (let i = 0; i < cuadrados.length; i++) {
+  cuadrados[i].addEventListener('click', function () {
+    clickedColor = cuadrados[i].style['background-color'];
+    if (pickedColor == clickedColor) {
+      changeColors(clickedColor);
+    } else {
+      span_message.textContent = "Inténtelo nuevamente";
+      cuadrados[i].style['background-color'] = '#232323';
+    }
+  });
+}
