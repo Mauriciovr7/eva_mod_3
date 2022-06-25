97fbdf7f1ef3559457a564065af72a23da4e5168
maria victoria <vipurplerose@gmail.com>
2022-06-25 18:58:57 -0400

    comentarios,revision,orden de acuerdo a lo pedido en el pdf

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Variable html//
 const titulo = document.querySelector('h1')
-const cuadrados = document.querySelectorAll('.square')
+const cuadrados = document.querySelectorAll('.square') //variable square//
 const span_color = document.querySelector('#colorDisplay')
 const btn_reset = document.querySelector('#reset')
 const btn_hard = document.querySelector('#dificil')
@@ -13,45 +13,59 @@ const lost = "Inténtelo nuevamente"
 let span_message = document.querySelector("#message")
 
 //variables globales//
-let colors = []
-let pickedColor = undefined;
-let clickedColor = undefined;
+let colors = [] //Arreglo colors//
+let pickedColor = undefined; //variable color aleatorio del computador//
+let clickedColor = undefined;// color seleccionado por usuario//
 let cant_cuadrados = 6
 
+/* function Desdeafuera (color){
+  for (let i = 0; i < cuadrados.length; i++) {
+  cuadrados[i].style['background-color'] = color;//parámetro color ganador//
+  titulo.style['color'] = color;
+  span_color.textContent = color;
+  }
+} */
 // Funcion cambio color
-function changeColors(color) {
+   function changeColors(color) {
   for (let i = 0; i < cuadrados.length; i++) {
     span_message.textContent = win; // ganador ***
     btn_reset.textContent = 'Play Again?'
     btn_reset.addEventListener('click', function () {
       btn_reset.textContent = 'Nuevos Colores'
       span_message.textContent = ''
+      x= undefined
     });
-    cuadrados[i].style['background-color'] = color;
+    cuadrados[i].style['background-color'] = color; //parámetro color ganador//
     titulo.style['color'] = color;
     span_color.textContent = color;
   }
   msg_voz.text = win
   window.speechSynthesis.speak(msg_voz)
-}
+} 
 
 // Funcion para retornar color aleatorio
 function pickColor() {
-  pickedColor = Math.floor(Math.random() * colors.length);
+  pickedColor = Math.floor(Math.random() * colors.length); //número de posición del color//
+  console.log (pickedColor)
   return pickedColor;
 }
 
 
 // genera 1 color al azar rgb(21,34,78)
-function randomColor() {
+function randomColor(x) {
   for (let i = 0; i < colors.length; i++) { // colors.length
     console.log('colors ', colors.length)//6
     // console.log(cuadrados.length)//5
+    console.log (x)//undefined
+    /* if (x==undefined){
+      cuadrados[i].style.visibility = 'visible'
+    } */
     cuadrados[i].style.visibility = 'visible'
-    cuadrados[i].style['background-color'] = colors[i]
+    cuadrados[i].style['background-color'] = colors[i] //variable i para cuadrados y color//
   }
-  
-  return pickedColor = colors[pickColor()];
+
+
+  return pickedColor = colors[pickColor()];//retorna color en la posición arrojada por pickColor//
 }
 // genera un array de N colores al azar
 function generateRandomColors(n) { // n = 6(hard) ó 3(easy) cuadros *****
@@ -79,9 +93,11 @@ randomColor();
 // jugar
 for (let i = 0; i < cuadrados.length; i++) {
   console.log('cuadrados ',cuadrados.length)
-  cuadrados[i].addEventListener('click', function () {
+  cuadrados[i].addEventListener('click', function () { // color seleccionado por el usuario//
     clickedColor = cuadrados[i].style['background-color'];
-    if (pickedColor == clickedColor) {
+    if (pickedColor == clickedColor) { //condición para ganar//
+      span_message.textContent = win; // ganador ***
+     
       changeColors(clickedColor); // gana
     } else {
       span_message.textContent = lost // pierde
